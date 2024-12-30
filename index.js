381aa9c6aa6d6de475ea42c4a25bf0d40e48c751
shazaniyu55 <shazaniyu@gmail.com>
2024-12-30 09:31:53 +0100

    new

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors({
-    origin: "https://hacked-backend.vercel.app/",          // Removed the trailing slash
+    origin: "http://localhost:2300/",          // Removed the trailing slash
     methods: 'GET, POST, PUT, DELETE',       // Methods allowed
     allowedHeaders: 'Content-Type, Authorization' // Corrected 'authorization' to 'Authorization'
   }));
